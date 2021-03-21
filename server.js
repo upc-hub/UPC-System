3f190e9052df915637e5325a279b9c686b649f47
Hein Htet <79504426+upc-hub@users.noreply.github.com>
2021-03-21 10:15:29 +0900

    Add web server js

---
+++ server.js
@@ -0,0 +1,130 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+let port = process.argv[2] || 1200;
+const httpServer = http.createServer(requestHandler);
+httpServer.listen(port, () => {console.log('server is listening on port '+ port)});
+
+function requestHandler(req, res){
+  if(req.url === '/'){
+    sendIndexHtml(res);
+  }else if( req.url === '/list'){
+    sendListOfUploadedFiles(res);
+  }else if( /\/submit_download\/[^\/]+$/.test(req.url)){
+    sendUploadedFile(req.url, res);
+  }else if( /\/upload\/[^\/]+$/.test(req.url) ){
+    saveUploadedFile(req, res)
+  }else if( /\/execute\/[^\/]+$/.test(req.url) ){
+    executeUploadedFile(req, res)
+  }else{
+    sendInvalidRequest(res);
+  }
+}
+
+function sendIndexHtml(res){
+  let indexFile = path.join(__dirname, 'index.html');
+  fs.readFile(indexFile, (err, content) => {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text'});
+      res.write('File Not Found!');
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(content);
+      res.end();
+    }
+  })
+}
+
+function sendListOfUploadedFiles(res){
+  let uploadDir = path.join(__dirname, 'submit_download');
+  fs.readdir(uploadDir, (err, files) => {
+    if(err){
+      console.log(err);
+      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(err.message));
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(files));
+      res.end();
+    }
+  })
+}
+
+
+function sendUploadedFile(url, res){
+  let file = path.join(__dirname, url);
+  fs.readFile(file, (err, content) => {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text'});
+      res.write('File Not Found!');
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+      res.write(content);
+      res.end();
+    }
+  })
+}
+
+
+function saveUploadedFile(req, res){
+  console.log('saving uploaded file');
+  let fileName = path.basename(req.url);
+  let file = path.join(__dirname, 'submit_download', fileName)
+  req.pipe(fs.createWriteStream(file));
+  req.on('end', () => {
+    res.writeHead(200, {'Content-Type': 'text'});
+    res.write('uploaded succesfully');
+    res.end();
+  })
+}
+
+function executeUploadedFile(req, res){
+  console.log('executing uploaded file');
+  let fileName = path.basename(req.url);
+  let file = path.join(__dirname, 'submit_download', fileName)
+  req.pipe(fs.createWriteStream(file));
+  req.on('end', () => {
+    res.writeHead(200, {'Content-Type': 'text'});
+    res.write('Executing.....');
+    res.end();
+  })
+  var delayInMilliseconds = 30000; //1 second
+let {PythonShell} = require('python-shell')
+setTimeout(function() {
+  //the following line calls the UPC master program so you will see the error when you click the last job button(that time you don't consider UPC master side.)
+  PythonShell.run('/home/heinhtet/Desktop/Systematic-1/new_complete/UPC_test/migratetesta.py', null, function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
+}, delayInMilliseconds);
+  let pyshell = new PythonShell('script1.py');
+
+// sends a message to the Python script via stdin
+pyshell.send('hello');
+
+pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err,code,signal) {
+  if (err) throw err;
+  console.log('The exit code was: ' + code);
+  console.log('The exit signal was: ' + signal);
+  console.log('finished');
+});
+  console.log('executing uploaded file 2nd');
+  
+    
+}
+
+function sendInvalidRequest(res){
+  res.writeHead(400, {'Content-Type': 'application/json'});
+  res.write('Invalid Request');
+  res.end(); 
+}
